6135e3903ddc721f82e13052696b4bd769e2574e
soonmin-kwon <tnsals0709@naver.com>
2021-03-15 19:41:47 +0900

    Springboot_2nd day

---
+++ src/test/java/org/experience/k/springboot/domain/posts/PostRepositoryTest.java
@@ -0,0 +1,40 @@
+package org.experience.k.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void CleanUp(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Load(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author("tnsals0709@gmail.com").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
